a40f83fc0a42bbe1f2dc832f76cc37fca9a009a1
APT43 <cabra@higashi.localdomain>
2023-07-27 20:29:55 +0300

    simple shell addendum

--- shell.h
+++ shell.h
@@ -5,19 +5,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
-/* for read/write buffers */
+/* for ro/rw buffer */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
-/* for command chaining */
+/* for combining commands */
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
@@ -122,7 +122,7 @@ int is_cmd(info_t *, char *);
 char *dup_chars(char *, int, int);
 char *find_path(info_t *, char *, char *);
 
-/* loophsh.c */
+/* loop_hsh.c */
 int loophsh(char **);
 
 /* toem_errors.c */
